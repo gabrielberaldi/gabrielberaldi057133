94ebd0ca9464cadc21e70586e0657404d23734b2
Gabriel Beraldi <gabriel.beraldi@hotmail.com>
2026-01-25 15:56:02 -0400

    fix(auth): fix facade logic

--- src/app/core/auth/facades/auth.facade.ts
+++ src/app/core/auth/facades/auth.facade.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from "@angular/core";
 import { AuthService } from "../services/auth.service";
 import { UserCredentials } from "../models/user-credentials.model";
-import { BehaviorSubject, Observable, tap } from "rxjs";
+import { BehaviorSubject, map, Observable, tap, throwError } from "rxjs";
 import { AuthResponse } from "../models/auth-response.model";
 import { Router } from "@angular/router";
 
@@ -13,36 +13,36 @@ export class AuthFacade {
   
   private readonly authKey = 'auth_data';
   private readonly authDataSubject$ = new BehaviorSubject<AuthResponse | null>(this.loadFromStorage());
+
+  readonly isAuthenticated$ = this.authDataSubject$.pipe(map(data => !!data?.access_token)); 
   
   get accessToken(): string | null {
     const authData = this.authDataSubject$.getValue();
     return authData ? authData.access_token : null;
   }
 
-  login(userCredentials: UserCredentials) {
+  login(userCredentials: UserCredentials): Observable<AuthResponse> {
     return this.authService.login(userCredentials).pipe(
       tap(response => {
         this.updateSession(response);
         this.router.navigate(['/layout']);
       })
-    ).subscribe();
+    )
   }
 
   logout(): void {
     this.authDataSubject$.next(null);
-    localStorage.removeItem('access_token');
+    localStorage.removeItem(this.authKey);
     this.router.navigate(['/login']);
   }
 
   refreshToken(): Observable<AuthResponse> { 
     const refreshToken = this.authDataSubject$.getValue()?.refresh_token;
-    if (!refreshToken) throw new Error('Sem refresh token disponível');
+    if (!refreshToken) return throwError(() => new Error('Sem refresh token disponível'));
     return this.authService.refreshToken(refreshToken).pipe
       (tap(response => this.updateSession(response))
     );
   }
-
-  
   private loadFromStorage(): AuthResponse | null { 
     const saved = localStorage.getItem(this.authKey);
     return saved ? JSON.parse(saved) : null;
