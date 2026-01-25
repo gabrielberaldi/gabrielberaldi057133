801c550de869113119af18f6ada9196ca21e77e2
Gabriel Beraldi <gabriel.beraldi@hotmail.com>
2026-01-24 23:57:54 -0400

    feat(shared) implement custom input component

---
+++ src/app/shared/components/input/input.component.ts
@@ -0,0 +1,72 @@
+import { Component, inject, Input, Optional, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [ ReactiveFormsModule ],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss',
+})
+export class InputComponent implements ControlValueAccessor {
+
+  @Input({ required: true }) label!: string;
+  @Input() placeholder: string = '';
+  @Input({ required: true }) type!: 'text' | 'password';
+
+  @Self() @Optional() protected ngControl = inject(NgControl, { self: true, optional: true });
+
+  protected disabled = false;
+  
+  private errorMessages: Record<string, any> = {
+    email: 'E-mail inválido',
+    required: 'Campo obrigatório'
+  };
+  
+  private value = '';
+
+  onChange: any = () => {};
+  onTouched: any = () => {}
+
+  constructor() {
+    if (!!this.ngControl) {
+      this.ngControl.valueAccessor = this;
+    }
+  }
+
+  get isInvalid(): boolean { 
+    const control = this.ngControl?.control;
+    return !!control && control.invalid && (control.dirty || control.touched);
+  }
+
+  getErrorMessage(): string { 
+    const errors = this.ngControl?.control?.errors;
+    if (!errors) return '';
+    const firstErrorKey = Object.keys(errors)[0];
+    return this.errorMessages[firstErrorKey];
+  }
+
+  onInput(event: Event): void { 
+    const value = (event.target as HTMLInputElement).value;
+    this.value = value;
+    this.onChange(value);
+  }
+
+  registerOnChange(fn: any): void { 
+    this.onChange = fn; 
+  }
+
+  registerOnTouched(fn: any): void { 
+    this.onTouched = fn; 
+  }
+
+  setDisabledState(isDisabled: boolean): void { 
+    this.disabled = isDisabled; 
+  }
+  
+  writeValue(value: any): void { 
+    this.value = value;
+  }
+  
+
+}
