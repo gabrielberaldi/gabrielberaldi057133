e3564b46f66868aacc21947a7d042a30ca38572a
Gabriel Beraldi <gabriel.beraldi@hotmail.com>
2026-01-26 19:53:22 -0400

    feat(shared): implements breadcrumb component

---
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { Breadcrumb } from './models/breadcrumb.model';
+import { RouterLink } from '@angular/router';
+import { ArrowLeftIcon, LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-breadcrumb',
+  standalone: true,
+  imports: [RouterLink, LucideAngularModule],
+  templateUrl: './breadcrumb.component.html',
+  styleUrl: './breadcrumb.component.scss'
+})
+export class BreadcrumbComponent {
+
+  @Input({ required: true }) items!: Breadcrumb[];
+
+  protected readonly ArrowLeftIcon = ArrowLeftIcon;
+
+}
