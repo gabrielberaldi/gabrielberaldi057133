9c31b31ba066c0559894aa39f723a133ea1ea6cc
Gabriel Beraldi <gabriel.beraldi@hotmail.com>
2026-01-27 16:19:35 -0400

    feat(paginator): create paginator component

---
+++ src/app/shared/components/paginator/paginator.component.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-paginator',
+  standalone: true,
+  imports: [],
+  templateUrl: './paginator.component.html',
+  styleUrl: './paginator.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PaginatorComponent {
+
+  @Input({ required: true }) currentPage = 0;
+  @Input({ required: true }) totalElements = 0;
+  @Input({ required: true }) totalPages = 0;
+
+  @Output() pageChange = new EventEmitter<number>();
+
+  protected get pages(): number[] {
+    const pages = [];
+    const start = Math.max(0, this.currentPage - 1);
+    const end = Math.min(this.totalPages - 1, this.currentPage + 1);
+
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+    return pages;
+  }
+
+  protected onPageChange(page: number): void {
+    if (page >= 0 && page < this.totalPages) {
+      this.pageChange.emit(page);
+    }
+  }
+
+}
