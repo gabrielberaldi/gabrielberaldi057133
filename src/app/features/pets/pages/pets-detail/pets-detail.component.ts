d0f45b53893d58fdd58e579628ea3c35016c8afb
Gabriel Beraldi <gabriel.beraldi@hotmail.com>
2026-01-31 19:31:36 -0400

    refactor(pet-details): refactor ui using shared components

--- src/app/features/pets/pages/pets-detail/pets-detail.component.ts
+++ src/app/features/pets/pages/pets-detail/pets-detail.component.ts
@@ -1,30 +1,30 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
-import { ArrowLeft, ExternalLink, LucideAngularModule, PawPrint, Pencil, Phone, Users } from 'lucide-angular';
+import { LucideAngularModule, Phone, User, Users } from 'lucide-angular';
 import { PetsFacade } from '../../facades/pets.facade';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { ImagePlaceholderComponent } from '../../../../shared/components/image-placeholder/image-placeholder.component';
-import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { ShellFacade } from '../../../../core/facades/shell.facade';
+import { DetailContainerComponent } from '../../../../shared/components/detail-container/detail-container.component';
+import { filter, map, switchMap, tap } from 'rxjs';
+import { InfoCardComponent } from '../../../../shared/components/info-card/info-card.component';
 
 @Component({
   selector: 'app-pets-detail',
   standalone: true,
-  imports: [AsyncPipe, ButtonComponent, ImagePlaceholderComponent, LucideAngularModule, RouterLink],
+  imports: [AsyncPipe, DetailContainerComponent, InfoCardComponent, LucideAngularModule],
   templateUrl: './pets-detail.component.html',
   styleUrl: './pets-detail.component.scss'
 })
 export class PetsDetailComponent implements OnInit, OnDestroy {
-
+  
   protected readonly petsFacade = inject(PetsFacade);
   private readonly activatedRoute = inject(ActivatedRoute);
   private readonly destroyRef = inject(DestroyRef);
+  private readonly shellFacade = inject(ShellFacade);
 
-  protected readonly ArrowLeft = ArrowLeft;
-  protected readonly ExternalLink = ExternalLink;
-  protected readonly Paw = PawPrint;
-  protected readonly Pencil = Pencil;
   protected readonly Phone = Phone;
+  protected readonly User = User;
   protected readonly Users = Users;
 
   protected id!: number;
@@ -40,18 +40,24 @@ export class PetsDetailComponent implements OnInit, OnDestroy {
 
   private initRouteListener(): void {
     this.activatedRoute.paramMap
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(params => {
-        const id = params.get('id');
-        if (id) {
-          this.petsFacade.loadPet(Number(id)).subscribe();
-        } else {
-          this.petsFacade.clearPet();
-        }
-      }
-    );
+      .pipe(
+        map(params => Number(params.get('id'))),
+        filter(id => !!id),
+        switchMap(id => this.petsFacade.loadPet(id)), 
+        tap(pet => {
+          if (pet) {
+            this.shellFacade.setBreadCrumbs({ 
+              breadcrumbs: [
+                { label: 'Pets', link: '/shell/pets' },
+                { label: 'Detalhes' }, 
+                { label: pet.nome } 
+              ] 
+            });
+          }
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe();
   }
-
-  
 
 }
