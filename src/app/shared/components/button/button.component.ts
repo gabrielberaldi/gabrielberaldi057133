c0e1ec6fb1324a695ee1a2ed81c2d459ab73debf
Gabriel Beraldi <gabriel.beraldi@hotmail.com>
2026-01-31 15:42:52 -0400

    refactor(button): move variant class logic to typescript

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -23,7 +23,19 @@ export class ButtonComponent {
 
   @Output() action = new EventEmitter<MouseEvent>();
 
-  onClick(event: MouseEvent) {
+  private readonly variants: Record<string, string> = {
+    'btn-primary': 'btn-outline btn-primary',
+    'btn-outline': 'btn-outline hover:bg-base-200 font-medium shadow-none',
+    'btn-error': 'btn-outline btn-error',
+    'btn-neutral': 'btn-neutral'
+  }
+
+  get variantClasses(): string {
+    const baseClasses = 'btn gap-2 font-semibold shadow-md';
+    return `${baseClasses} ${this.variants[this.variant] || ''}`;
+  }
+
+  protected onClick(event: MouseEvent) {
     if (!this.disabled && !this.loading) {
       this.action.emit(event);
     }
