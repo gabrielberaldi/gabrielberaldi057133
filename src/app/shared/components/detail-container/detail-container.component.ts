de45f59023a2a4b7c738216dc1d6d0fc782aa576
Gabriel Beraldi <gabriel.beraldi@hotmail.com>
2026-02-01 17:55:21 -0400

    refactor(detail-container): decoupling DetailContainer using outputs instead of routerlinks

--- src/app/shared/components/detail-container/detail-container.component.ts
+++ src/app/shared/components/detail-container/detail-container.component.ts
@@ -1,31 +1,44 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ImagePlaceholderComponent } from '../image-placeholder/image-placeholder.component';
 import { ButtonComponent } from '../button/button.component';
-import { RouterLink } from '@angular/router';
 import { ArrowLeft, ExternalLink, LucideAngularModule, LucideIconData, PawPrint, Pencil, User } from 'lucide-angular';
 
 @Component({
   selector: 'app-detail-container',
   standalone: true,
-  imports: [ ButtonComponent, ImagePlaceholderComponent, LucideAngularModule, RouterLink ],
+  imports: [ ButtonComponent, ImagePlaceholderComponent, LucideAngularModule ],
   templateUrl: './detail-container.component.html',
   styleUrl: './detail-container.component.scss'
 })
 export class DetailContainerComponent {
-  @Input({ required: true }) backLink!: string;
-  @Input({ required: true }) editLink!: any[];
+
   @Input() imageUrl?: string;
   @Input() imagePlaceholder: LucideIconData = PawPrint; 
-  @Input() listItems: any[] | undefined = [];
+  @Input() listItems: any[] = [];
   @Input() listItemImagePlaceholder: LucideIconData = PawPrint;
   @Input() listTitle: string = 'Vínculos';
   @Input() listIcon: LucideIconData = User;
-  @Input() listDetailRoutePrefix: string = '';
   @Input() listSubtitleProperty: string = '';
   @Input({ required: true }) title!: string;
+  
+  @Output() back = new EventEmitter<void>();
+  @Output() edit = new EventEmitter<void>();
+  @Output() itemClick = new EventEmitter<number>();
 
   protected readonly ArrowLeft = ArrowLeft;
   protected readonly ExternalLink = ExternalLink;
   protected readonly PawPrint = PawPrint;
   protected readonly Pencil = Pencil;
+
+  protected onBack(): void {
+    this.back.emit();
+  }
+
+  protected onEdit(): void {
+    this.edit.emit();
+  }
+
+  protected onItemClick(id: number): void {
+    this.itemClick.emit(id);
+  }
 }
