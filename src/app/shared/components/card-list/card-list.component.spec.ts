55d9259f051575a2398bd0fd1dd1043405de20c6
Gabriel Beraldi <gabriel.beraldi@hotmail.com>
2026-02-04 14:30:42 -0400

    test: implement test for carrd-list-component

--- src/app/shared/components/card-list/card-list.component.spec.ts
+++ src/app/shared/components/card-list/card-list.component.spec.ts
@@ -14,10 +14,35 @@ describe('CardListComponent', () => {
 
     fixture = TestBed.createComponent(CardListComponent);
     component = fixture.componentInstance;
+    component.list = {
+      content: [],
+      total: 0,
+      page: 0,
+      pageCount: 0,
+      size: 0
+    };
+    component.cardTemplate = {} as any;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('shoud emit serch event when searchControl value changes', (done) => {
+    spyOn(component.search, 'emit');
+    component['searchControl'].setValue('teste');
+    setTimeout(() => {
+      expect(component.search.emit).toHaveBeenCalledWith('teste');
+      done();
+    }, 150);
+  });
+
+  it('shoud emit pageChange event',() => {
+    spyOn(component.pageChange, 'emit');
+    const mockPage = 2;
+    component['onPageChanged'](mockPage);
+    expect(component.pageChange.emit).toHaveBeenCalledWith(mockPage);
+  });
+
 });
